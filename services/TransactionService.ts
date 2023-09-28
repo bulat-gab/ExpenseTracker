6178e1e7d7b68801d7080a7dea555fa4fa525884
bulat-gab <gabdrakhmanov.br@gmail.com>
2023-09-28 13:01:46 +0700

    Cache total amount in Tx service

--- services/TransactionService.ts
+++ services/TransactionService.ts
@@ -1,14 +1,19 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Transaction from '../models/Transaction';
 
+let totalAmount: Number = -1;
+
 const GetTransactionsAsync = async () => {
   try {
     const allKeys: readonly string[] = await AsyncStorage.getAllKeys();
     if (allKeys === null || allKeys.length === 0) {
+      totalAmount = 0;
       return [];
     }
 
     const transactionsFromDb = [];
+    let totalAmountTemp = 0;
+
     for (let i = 0; i < allKeys.length; i++) {
       const tx = await AsyncStorage.getItem(allKeys[i]);
       if (tx === null) {
@@ -18,18 +23,32 @@ const GetTransactionsAsync = async () => {
       const txObject = JSON.parse(tx);
       const txTyped: Transaction = Transaction.createFromObject(txObject);
       transactionsFromDb.push(txTyped);
+      totalAmountTemp += txTyped.getAmount();
     }
 
     console.log('Expenses: ', transactionsFromDb);
+    totalAmount = totalAmountTemp;
     return transactionsFromDb;
   } catch (error) {
     console.error('Error:', error);
+    totalAmount = 0;
     return [];
   }
 };
 
+const GetTotalAmountAsync = async () => {
+  if (totalAmount !== -1) {
+    return totalAmount;
+  }
+
+  await GetTransactionsAsync();
+  console.log('Total amount: ', totalAmount);
+  return totalAmount;
+};
+
 const transactionService = {
   GetTransactionsAsync,
+  GetTotalAmountAsync,
 };
 
 export default transactionService;
